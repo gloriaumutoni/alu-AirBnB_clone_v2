3180874ee486cc9173293a7304d7ffd6ae820049
Gloria Umutoni <124312203+gloriaumutoni@users.noreply.github.com>
2024-10-05 19:08:55 +0200

    pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,24 +1,19 @@
 #!/usr/bin/python3
-"""
-    script that generates '.tgz' archive from the contents of the 'web_static'
-"""
-from fabric.api import local
+"""Fabric script which generates a tgz archive"""
+
 from datetime import datetime
+from fabric.api import local
+from os.path import isdir
 
 
 def do_pack():
-    """
-        function to compress directory into .tgz archive
-        Return: Success - '.tgz' archive path
-                Failure - None
-    """
-    now = datetime.now()
-    now = now.strftime('%Y%m%d%H%M%S')
-    archive_path = 'versions/web_static_' + now + '.tgz'
-
-    local('mkdir -p versions/')
-    result = local('tar -cvzf {} web_static/'.format(archive_path))
-
-    if result.succeeded:
-        return archive_path
-    return None
+    """TGZ"""
+    try:
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        if isdir("versions") is False:
+            local("mkdir versions")
+        file_name = "versions/web_static_{}.tgz".format(date)
+        local("tar -cvzf {} web_static".format(file_name))
+        return file_name
+    except:
+        return None
